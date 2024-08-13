51d6f17a517c90948c4ae6db00073063bc897e54
Ariel Barbosa Santos <ab927790@gmail.com>
2024-08-12 21:47:25 -0300

    commit

--- exercRevisao.py
+++ exercRevisao.py
@@ -1,46 +1,42 @@
-# agenda 
+# agenda
+import nome
 
+# criando lista 'agenda'
 agenda = []
 
-alterada = False # Variável para marcar uma alteração na agenda
+# Variável para marcar uma alteração na agenda
+alterada = False
+
 
 # função pede nome
 # determina o padrão de nome que o usuario deve digitar
-def pede_nome(padrão = ""):
+def pede_nome(padrao = ""):
     nome = input("Nome: ")
-
-if nome == "":
-    nome = padrão
-    return nome
-
+    if nome == "":
+        nome = padrao
+        return nome
 
 # determina o padrão de telefone que o usuario deve digitar
-def pede_telefone(padrão = ""):
+def pede_telefone(padrao = ""):
     telefone = input("Telefone: ")
-
-if telefone == "":
-    telefone = padrão
-    return telefone
-
-
+    if telefone == "":
+        telefone = padrao
+        return telefone
 
 # determina o padrão de email que o usuario deve digitar
-def pede_email(padrão=""):
+def pede_email(padrao = ""):
     email = input("Email: ")
-
-if email == "":
-    email = padrão
-    return email
-
+    if email == "":
+        email = padrao
+        return email
 
 
 # determina o padrão de aniversario que o usuario deve digitar
-def pede_aniversário(padrão=""):
-    aniversário = input("Data de aniversário: ")
-
-if aniversário == "":
-    aniversário = padrão
-    return aniversario
+def pede_aniversário(padrao=""):
+    aniversario = input("Data de aniversário: ")
+    if aniversario == "":
+        aniversário = padrao
+        return aniversario
 
 
 # mostra os dados do usuario
@@ -49,4 +45,85 @@ def mostra_dados(nome, telefone, email, aniversario):
         f"Nome: {nome}\nTelefone: {telefone}\n"
         f"Email: {email}\nAniversário: {aniversario}\n"
     )
+
+# funcao que pede o nome do arquivo
+def pede_nome_arquivo():
+    return input("Nome do Arquivo: ")
+
+def pesquisa(nome):
+    mnome = nome.lower()
+    for p, e in enumerate(agenda):
+        if e[0].lower() == mnome:
+            return p
+            return None
+
+def novo():
+    global agenda, alterada
+    nome = pede_nome()
+    if pesquisa() is not None:
+        print("Nome já existe!")
+        return
+        telefone = pede_telefone()
+        agenda.append([nome, telefone, email, aniversario])
+        alterada = True
+
+def confirma(operacao):
+    while True:
+        opcao = input(f"Confirma {operacao} (S/N)? ").upper()
+        if opcao in "SN":
+            return opcao
+        else:
+            print("Resposta invalida. Escolha S ou N. ")
+
+def apaga():
+    global agenda, alterada
+    nome = pede_nome()
+    p = pesquisa(nome)
+    if p is not None and confirma("apagamento") == "S":
+        del agenda[p]
+        alterada = True
+    else:
+        print("Nome não encontrado... ")
+
+def altera():
+    global alterada
+    p = pesquisa(pede_nome())
+    if p is not None:
+        nome = agenda[p][0]
+        telefone = agenda[p][1]
+        email = agenda[p][2]
+        aniversário = agenda[p][3]
+        print("Encontrado:")
+        mostra_dados(nome, telefone, email, aniversário)
+        nome = pede_nome(nome)  # Se nada for digitado, mantém o valor
+        telefone = pede_telefone(telefone)
+        email = pede_email(email)
+        aniversário = pede_aniversário(aniversário)
+
+        if confirma("alteração") == "S":
+            agenda[p] = [nome, telefone, email, aniversário]
+            alterada = True
+        else:
+            print("Nome não encontrado.")
+
+
+def lista():
+    print("\nAgenda\n\n\------")
+
+    # Usamos a função enumerate para obter a posição na agenda
+    for posicao, 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
