642e3072cae432a33699a6092d62051bdc5c6f75
Ariel Barbosa Santos <ab927790@gmail.com>
2024-08-13 16:19:13 -0300

    commit

--- exercRevisao.py
+++ exercRevisao.py
@@ -1,7 +1,10 @@
 # agenda
-import nome
+
 
 # criando lista 'agenda'
+from pickle import FALSE
+
+
 agenda = []
 
 # Variável para marcar uma alteração na agenda
@@ -10,11 +13,10 @@ alterada = False
 
 # função pede nome
 # determina o padrão de nome que o usuario deve digitar
-def pede_nome(padrao = ""):
+def pede_nome(padrao=""):
     nome = input("Nome: ")
-    if nome == "":
-        nome = padrao
-        return nome
+    return nome or ""  # Retorna nome se não for vazio, senão retorna ""
+
 
 # determina o padrão de telefone que o usuario deve digitar
 def pede_telefone(padrao = ""):
@@ -55,12 +57,12 @@ def pesquisa(nome):
     for p, e in enumerate(agenda):
         if e[0].lower() == mnome:
             return p
-            return None
+        return None
 
 def novo():
     global agenda, alterada
     nome = pede_nome()
-    if pesquisa() is not None:
+    if pesquisa(nome) is not None:
         print("Nome já existe!")
         return
         telefone = pede_telefone()
@@ -111,7 +113,149 @@ def lista():
     print("\nAgenda\n\n\------")
 
     # Usamos a função enumerate para obter a posição na agenda
-    for posicao, 
+    for posição, e in enumerate(agenda):
+        print(f"\nPosição: {posição}") # Imprimimos a posição
+        mostra_dados(e[0], e[1], e[2], e[3])
+        print("\------\n")
+
+def lê_última_agenda_gravada():
+    ultima = ultima_agenda()
+    if ultima is not None:
+        leia_arquivo(ultima)
+
+
+def ultima_agenda():
+    try:
+        arquivo = open("ultima agenda.dat", "r", encoding="utf-8")
+        ultima = arquivo.readline()[:-1]
+        arquivo.close()
+    except FileExistsError:
+        return None
+        return ultima
+    
+
+def atualiza_ultima(nome):
+    arquivo = open("ultima agenda.dat", "w", encoding="utf-8")
+    arquivo.write(f"{nome}\n")
+    arquivo.close()
+
+
+def leia_arquivo(nome_arquivo):
+    global agenda, alterada
+    arquivo = open(nome_arquivo, "r", encoding="utf-8")
+    agenda = []
+
+    for l in arquivo.readlines():
+        nome, telefone, email, aniversário = l.strip().split("#")
+        agenda.append([nome, telefone, email, aniversário])
+        arquivo.close()
+        alterada = False
+
+
+def le():
+    global alterada
+    if alterada:
+        print("Você não salvou a lista desde a última alteração. Deseja gravá-la agora?")
+
+    if confirma("gravação") == "S":
+        grava()
+        print("Ler\n---")
+        nome_arquivo = pede_nome_arquivo()
+        leia_arquivo(nome_arquivo)
+        atualiza_ultima(nome_arquivo)
+
+def ordena():
+    global alterada
+    # Você pode ordenar a lista como mostrado no livro
+    # com o método de bolhas (bubble sort)
+    # Ou combinar o método sort do Python com lambdas para
+    # definir a chave da lista
+    # agenda.sort(key=lambda e: return e[0])
+
+    fim = len(agenda)
+
+    while fim > 1:
+        i = 0
+
+    trocou = False
+
+    while i < (fim - 1):
+        if agenda[i] > agenda[i + 1]:
+            # Opção: agenda[i], agenda[i+1] = agenda[i+1], agenda[i]
+            temp = agenda[i + 1]
+            agenda[i + 1] = agenda[i]
+            agenda[i] = temp
+            trocou = True
+            i += 1
+
+            if not trocou:
+                break
+            altera = True
+
+
+def grava():
+    global alterada
+    if not alterada:
+        print("Você não alterou a lista. Deseja gravá-la mesmo assim?")
+        if confirma("gravação") == "N":
+            return 
+        print("Gravar\n\------")
+    nome_arquivo = pede_nome_arquivo()
+    arquivo = open(nome_arquivo, "w", encoding="utf-8")
+
+    for e in agenda:
+        arquivo.write(f"{e[0]}#{e[1]}#{e[2]}#{e[3]}\n")
+        arquivo.close()
+        atualiza_ultima(nome_arquivo)
+        alterada = False
+
+
+def valida_faixa_inteiro(pergunta, inicio, fim):
+    while True:
+        try:
+            valor = int(input(pergunta))
+            if inicio <= valor <= fim:
+                return valor
+        except ValueError:
+
+            print("Valor inválido, favor digitar entre {inicio} e {fim}")
+
+def menu():
+    print("""
+    1 - Novo
+    2 - Altera
+    3 - Apaga
+    4 - Lista
+    5 - Grava
+    6 - Lê
+    7 - Ordena por nome
+    0 - Sai
+    """)
+    print(f"\nNomes na agenda: {len(agenda)} Alterada: {alterada}\n")
+    return valida_faixa_inteiro("Escolha uma opção: ", 0, 7)
+    lê_última_agenda_gravada()
+
+while True:
+    opção = menu()
+    if opção == 0:
+        break
+    elif opção == 1:
+        novo()
+    elif opção == 2:
+        altera()
+    elif opção == 3:
+        apaga()
+    elif opção == 4:
+        lista()
+    elif opção == 5:
+        grava()
+    elif opção == 6:
+        le()
+    elif opção == 7:
+        ordena()
+
+                        
+
 
 
 
