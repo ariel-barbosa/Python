92c36b02dcec37c0b955c6a6cb2222e66242efa6
Ariel Barbosa Santos <ab927790@gmail.com>
2024-08-14 13:34:30 -0300

    commit

--- projetoExecutavel/funcoes.py
+++ projetoExecutavel/funcoes.py
@@ -1,36 +1,32 @@
-# def calcular_moda(lista):
-# 	frequencias = {}
-# 	for elemento in lista:
-# 		if elemento in frequencias:
-# 			frequencias[elemento] += 1
-# 		else:
-# 			frequencias[elemento] = 1
-# 			moda = []
-# 			maior_frequencia = 0
+def calcular_moda(lista):
+	frequencias = {}
+	for elemento in lista:
+		if elemento in frequencias:
+			frequencias[elemento] += 1
+		else:
+			frequencias[elemento] = 1
+			moda = []
+			maior_frequencia = 0
 
-# 		for elemento, frequencia in frequencias.items():
-# 			if frequencia > maior_frequencia:
-# 				moda = [elemento]
-# 				maior_frequencia = frequencia
-# 			elif:
-# 			requencia == maior_frequencia(moda.append(elemento))
-# 			return moda, maior_frequencia
-# # Entrada dos dados
+		for elemento, frequencia in frequencias.items():
+			if frequencia > maior_frequencia:
+				moda = [elemento]
+				maior_frequencia = frequencia
+			elif:
+			requencia == maior_frequencia(moda.append(elemento))
+			return moda, maior_frequencia
+# Entrada dos dados
 
-# lista = list()
-# i = 1
-# while i<=10:
-# 	elem = int(input("Digite um elemento da lista: "))
-# 	lista.append(elem)
-# 	i+=1
+lista = list()
+i = 1
+while i<=10:
+	elem = int(input("Digite um elemento da lista: "))
+	lista.append(elem)
+	i+=1
 
-# print(lista)
-# # Resultados
-# moda, frequencia = calcular_moda(lista)
-# print("Lista: ", lista)
-# print("Moda: ", moda)
-# print("Frequência: ", frequencia)
-
-
-number = 4
-print("Even" if number % 2 == 0 else "Odd")
+print(lista)
+# Resultados
+moda, frequencia = calcular_moda(lista)
+print("Lista: ", lista)
+print("Moda: ", moda)
+print("Frequência: ", frequencia)
